89997c8e1a3a52104419e0d48a57158150de9fda
ARJUNSV01 <101787633+ARJUNSV01@users.noreply.github.com>
2022-03-25 17:00:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const button = document.getElementById('iconsdiv1');
+const div = document.getElementById('IntelliSense');
+
+button.addEventListener('click', function() {
+   div.scrollIntoView();
+})
+const a=document.getElementById('iconsdiv2');
+const b=document.getElementById('Debug');
+
+a.addEventListener('click', function(){
+    b.scrollIntoView();
+})
+const c=document.getElementById('iconsdiv3');
+const d=document.getElementById('Git');
+
+c.addEventListener('click',function(){
+    d.scrollIntoView();
+})
+const e=document.getElementById('iconsdiv4');
+const f=document.getElementById('Extensions');
+
+e.addEventListener('click',function(){
+    f.scrollIntoView();
+})
